8106f7f609c94be9f2df148c90ead674d326b54b
Johannes Haux <johannes.haux@gmx.de>
2018-04-07 15:31:37 +0200

    Added Umbuchungs Comparator

--- beancountManager/deduplicate.py
+++ beancountManager/deduplicate.py
@@ -3,15 +3,79 @@ from beancount.parser import printer
 from beancount.ingest import similar
 from beancount.core.data import Transaction
 
+import datetime
+
 
 def find_and_delete_duplicates(l):
     for e1 in l:
         duplicates = similar.find_similar_entries([e1], l, window_days=4)
 
-        for d in duplicates:
+        for s, d in duplicates:
             if d != e1:
-                printer.print_entries([d[1]])
-                l.remove(d[1])
+                l.remove(d)
+
+    for e1 in l:
+        U = UmbuchungsComparator(datetime.timedelta(6))
+        duplicates_umbuchung = similar.find_similar_entries([e1],
+                                                            l,
+                                                            comparator=U,
+                                                            window_days=4)
+        for s, d in duplicates_umbuchung:
+            print(id(d))
+            printer.print_entries([d])
+            print(id(e1))
+            printer.print_entries([e1])
+            print('\n' + '='*20)
+
+            l.remove(d)
+
+
+class UmbuchungsComparator(object):
+    def __init__(self, max_date_delta=None):
+        self.max_date_delta = max_date_delta
+
+    def __call__(self, entry1, entry2):
+        if self.max_date_delta:
+            delta = ((entry1.date - entry2.date)
+                     if entry1.date > entry2.date else
+                     (entry2.date - entry1.date))
+        if delta > self.max_date_delta:
+            return False
+
+        try:
+            if entry1.meta['import_file'] != entry2.meta['import_file'] \
+                    and len(entry1.postings) == len(entry2.postings):
+                d1 = account_map(entry1)
+                d2 = account_map(entry2)
+                a1 = set(d1)
+                a2 = set(d2)
+
+                # Are all keys the same?
+                if len(a1 & a2) != len(entry1.postings):
+                    return False
+
+                # Are all postings to or from Assets?
+                for a in a1 | a2:
+                    if 'Assets:' not in a:
+                        return False
+
+                    if not d1[a] == d2[a]:
+                        return False
+
+                print(a1 | a2)
+
+                return True
+            return False
+
+        except Exception:
+            return False
+
+
+def account_map(e):
+    d = {}
+    for p in e.postings:
+        d[p.account] = (p.units.number, p.units.currency)
+    return d
 
 
 def n_um(l):
